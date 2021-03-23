ddfdb59bc1c511c66a2822d3806fc3bff56744ce
ylh23y126 <359639297@qq.com>
2021-03-23 21:42:42 +0800

    realm

---
+++ shiro-client/src/main/java/com/github/yun/shiro/oauth2/OAuth2Realm.java
@@ -0,0 +1,134 @@
+package com.github.yun.shiro.oauth2;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthBearerClientRequest;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author Administrator
+ * <p>Date:
+ * <p>Version: 1.0
+ */
+public class OAuth2Realm extends AuthorizingRealm {
+
+    private String clientId;
+    private String clientSecret;
+    private String accessTokenUrl;
+    private String userInfoUrl;
+    private String redirectUrl;
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public void setAccessTokenUrl(String accessTokenUrl) {
+        this.accessTokenUrl = accessTokenUrl;
+    }
+
+    public void setUserInfoUrl(String userInfoUrl) {
+        this.userInfoUrl = userInfoUrl;
+    }
+
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //表示此Realm只支持OAuth2Token类型
+        return token instanceof OAuth2Token;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        return authorizationInfo;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        OAuth2Token oAuth2Token = (OAuth2Token) token;
+        String code = oAuth2Token.getAuthCode();
+        String username = extractUsername(code);
+
+        SimpleAuthenticationInfo authenticationInfo =
+                new SimpleAuthenticationInfo(username, code, getName());
+        return authenticationInfo;
+    }
+    private static void trustAllHttpsCertificates() throws Exception {
+        TrustManager[] trustAllCerts = { new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {}
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {}
+        } };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    private String extractUsername(String code) {
+
+        try {
+            OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+
+            OAuthClientRequest accessTokenRequest = OAuthClientRequest
+                    .tokenLocation(accessTokenUrl)
+                    .setGrantType(GrantType.AUTHORIZATION_CODE)
+                    .setClientId(clientId)
+                    .setClientSecret(clientSecret)
+                    .setCode(code)
+                    .setRedirectURI(redirectUrl)
+                    .buildQueryMessage();
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession sslsession) {
+                    return true;
+                }
+            });
+            trustAllHttpsCertificates();
+            OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(accessTokenRequest, OAuth.HttpMethod.POST);
+
+            String accessToken = oAuthResponse.getAccessToken();
+            Long expiresIn = oAuthResponse.getExpiresIn();
+
+            OAuthClientRequest userInfoRequest = new OAuthBearerClientRequest(userInfoUrl)
+                    .setAccessToken(accessToken).buildQueryMessage();
+
+            OAuthResourceResponse resourceResponse = oAuthClient.resource(userInfoRequest, OAuth.HttpMethod.GET, OAuthResourceResponse.class);
+            String username = resourceResponse.getBody();
+            return username;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new OAuth2AuthenticationException(e);
+        }
+    }
+}

9db00c083cd9df8385a3462ecd2df07e3c89b5a4
ylh23y126 <359639297@qq.com>
2021-05-27 20:48:59 +0800

    Service

---
+++ shiro-server/src/main/java/com/github/yun/shiro/server/service/ClientServiceImpl.java
@@ -0,0 +1,60 @@
+package com.github.yun.shiro.server.service;
+
+import com.github.yun.shiro.server.dao.ClientDao;
+import com.github.yun.shiro.server.entity.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Administrator
+ * <p>Date:
+ * <p>Version: 1.0
+ */
+@Transactional
+@Service
+public class ClientServiceImpl implements ClientService {
+    @Autowired
+    private ClientDao clientDao;
+
+    @Override
+    public Client createClient(Client client) {
+
+        client.setClientId(UUID.randomUUID().toString());
+        client.setClientSecret(UUID.randomUUID().toString());
+        return clientDao.createClient(client);
+    }
+
+    @Override
+    public Client updateClient(Client client) {
+        return clientDao.updateClient(client);
+    }
+
+    @Override
+    public void deleteClient(Long clientId) {
+        clientDao.deleteClient(clientId);
+    }
+
+    @Override
+    public Client findOne(Long clientId) {
+        return clientDao.findOne(clientId);
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return clientDao.findAll();
+    }
+
+    @Override
+    public Client findByClientId(String clientId) {
+        return clientDao.findByClientId(clientId);
+    }
+
+    @Override
+    public Client findByClientSecret(String clientSecret) {
+        return clientDao.findByClientSecret(clientSecret);
+    }
+}

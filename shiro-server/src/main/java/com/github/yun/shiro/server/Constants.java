de7f7a00fb47a402269617b0303924d3bbaefb33
ylh23y126 <359639297@qq.com>
2021-06-23 21:26:08 +0800

    all

---
+++ shiro-server/src/main/java/com/github/yun/shiro/server/Constants.java
@@ -0,0 +1,12 @@
+package com.github.yun.shiro.server;
+
+/**
+ * @author Administrator
+ * <p>Date:
+ * <p>Version: 1.0
+ */
+public class Constants {
+
+    public static String RESOURCE_SERVER_NAME = "chapter17-server";
+    public static final String INVALID_CLIENT_DESCRIPTION = "客户端验证失败，如错误的client_id/client_secret。";
+}
